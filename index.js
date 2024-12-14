507dcaded28fe7246fdd6655a4f1b8832d012e05
mohamedsaliabdi <mohamedsaliabdi@gmail.com>
2024-12-14 13:14:18 -0600

    done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(event = 'roller-skate'){
+    return `This Saturday, I want to ${event}!`
+
+}
+
+function mondayWork(x = 'go to the office'){
+    return `This Monday, I will ${x}.`;
+}
+
+mondayWork('work from home')
+
+function wrapAdjective(wrapper = '*') {
+    return function result(adjective = '||') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
